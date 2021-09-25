be7a1af7933f75e6ec9d66457f146e3171bbbaa9
Petr Zakharov <zakharovp11@gmail.com>
2021-09-25 14:32:15 +0300

    add models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Follow(models.Model):
+    pass
+
+
+class Favorite(models.Model):
+    pass
+
+
+class ShoppingCart(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    pass
+
+
+class Ingredient(models.Model):
+    pass
+
+
+class IngredientAmount(models.Model):
+    pass
+
+
+class Recipe(models.Model):
+    pass
